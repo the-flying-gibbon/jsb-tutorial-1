a79b20aac9564b4f2bed94d91d0446779c1d214b
the-flying-gibbon <alessandro.santoni.fi@gmail.com>
2024-07-02 11:00:45 +0200

    Rename of main Application class

--- src/main/java/com/tutorial/mywebapp/Application.java
+++ src/main/java/com/tutorial/mywebapp/Application.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class MywebappApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MywebappApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 	
 	@GetMapping("api/greeting")
